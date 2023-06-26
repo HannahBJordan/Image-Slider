d7ca273a3465dfc579d1e77ce8220a55a371a863
Sung Mi Hong <92003748+HannahBJordan@users.noreply.github.com>
2023-06-25 20:02:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// var swiper = new Swiper(".swiper", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
